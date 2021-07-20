62928d0d9863dc1426e91510db8aa453039a43d1
Campagner, Martín <martincampagner@hotmail.com>
2021-07-20 03:02:06 -0300

    implementado getline + comentarios

--- sub.c
+++ sub.c
@@ -1,79 +1,97 @@
+#define _GNU_SOURCE
+
 #include "sub.h"
 #include "tiempo.h"
 
-//quiero crear la estructura para manejar los datos del archivo
-struct arreglo_sub *init_arreglo(struct arreglo_sub *a, int initial_size)
+// crea la estructura para manejar los datos del archivo
+struct arreglo_sub *init_arreglo(struct arreglo_sub *array, int initial_size)
 {
-	a->a = (struct sub *)malloc(initial_size * sizeof(struct sub));
-	a->tamanio = initial_size;
-	a->ocupado = 0;
-	return a;
+	array->a = (struct sub *)malloc(initial_size * sizeof(struct sub));
+	array->tamanio = initial_size;
+	array->ocupado = 0;
+	return array;
 }
 
-struct arreglo_sub *insert_sub(struct arreglo_sub *a, struct sub *dato)
+struct arreglo_sub *insert_sub(struct arreglo_sub *array, struct sub *dato)
 {
-	if (a->ocupado == a->tamanio)
+	// si está lleno, duplica el tamaño del arreglo de subtítulos
+	if (array->ocupado == array->tamanio)
 	{
-		a->tamanio *= 2;
-		a->a = (struct sub *)realloc(a->a, a->tamanio * sizeof(struct sub));
+		array->tamanio *= 2;
+		array->a = (struct sub *)realloc(array->a, array->tamanio * sizeof(struct sub));
 	}
-	a->a[a->ocupado++] = *dato;
-	return a;
+	// asigna el dato e incrementa la ocupación
+	array->a[array->ocupado++] = *dato;
+	return array;
 }
-
+// llena el arreglo inicial con los subtítulos del archivo *entrada
 struct arreglo_sub *inicializar(FILE *entrada)
 {
+	// inicializa arreglo de subtítulos
 	struct arreglo_sub *arreglo = calloc(1, sizeof(struct arreglo_sub));
 	arreglo = init_arreglo(arreglo, 1);
-	struct sub *dato;
-	char dato1[100];
-	int indice;
-	int i_hh = 0, i_mm = 0, i_ss = 0, i_ms = 0,
-		f_hh = 0, f_mm = 0, f_ss = 0, f_ms = 0;
 
-	//empiezo a leer
+
+	struct sub *dato;		// subtítulo auxiliar de lectura
+	char *texto = NULL;		// string de texto para getline()
+	size_t len = 0;			// caracteres leídos por getline()
+	int indice;				// índice auxiliar de subtítulos
+	int i_hh = 0, i_mm = 0, i_ss = 0, i_ms = 0,		// tiempos de inicio de subs
+		f_hh = 0, f_mm = 0, f_ss = 0, f_ms = 0;		// tiempos de fin de subs
+
+	// aloca el struct auxiliar
+	dato = malloc(sizeof(struct sub));
+
+	// empieza a leer
 
 	while (fscanf(entrada, "%d\n", &indice) != EOF)
 	{
-		dato = malloc(sizeof(struct sub));
 		dato->indice = indice;
 		fscanf(entrada, "%d:%d:%d,%d --> %d:%d:%d,%d\n",
 			   &i_hh, &i_mm, &i_ss, &i_ms,
 			   &f_hh, &f_mm, &f_ss, &f_ms);
+		
+		// convierte los datos numéricos a milisegundos
 		dato->inicio = tm_to_millisec(i_hh, i_mm, i_ss, i_ms);
 		dato->fin = tm_to_millisec(f_hh, f_mm, f_ss, f_ms);
-		dato->texto = malloc(sizeof(char) * 100);
-		while (3 != strcasecmp((fgets(dato1, 100, entrada)), "\n"))
-		{
-			//aca podria poner una sentencia de control para que haga realloc de ser necesario	!!!!
 
-			strcat(dato->texto, dato1);
+		// aloca el texto de entrada
+		dato->texto = malloc(sizeof(char) * 100);
+
+		// concatena hasta leer sólamente \n o \r\n 
+		while ((len = getline(&texto, &len, entrada)) > 2)
+		{
+			strcat(dato->texto, texto);
 		}
-		printf("%s", dato->texto);
+		
 		arreglo = insert_sub(arreglo, dato);
 	}
-	printf("Salio");
-	getchar();
+
+	// libera los punteros
+	free(dato);
+	if(texto != NULL)
+		free(texto);
 
 	return arreglo;
 }
 
-void crear_salida(struct arreglo_sub *sub, FILE **salida)
+// crea el archivo de salida en base al arreglo de subtitulos
+void crear_salida(struct arreglo_sub *array, FILE **salida)
 {
 	// dos estructuras t_tiempo, para inicio y fin
 	t_tiempo *inicio = (t_tiempo *)calloc(1, sizeof(t_tiempo));
 	t_tiempo *fin = (t_tiempo *)calloc(1, sizeof(t_tiempo));
 	
-	for (int i = 0; i < sub->ocupado - 1; i++)
+	for (int i = 0; i < array->ocupado - 1; i++)
 	{
-		inicio = millisec_to_tm(sub->a[i].inicio);
-		fin = millisec_to_tm(sub->a[i].fin);
+		inicio = millisec_to_tm(array->a[i].inicio);
+		fin = millisec_to_tm(array->a[i].fin);
 
-		fprintf(*salida, "%d\n", sub->a[i].indice); 
+		fprintf(*salida, "%d\n", array->a[i].indice); 
 		fprintf(*salida, "%02d:%02d:%02d,%03d --> %02d:%02d:%02d,%03d\n", 
 						inicio->hh, inicio->mm, inicio->ss, inicio->ms,
 						fin->hh	  , fin->mm   , fin->ss   , fin->ms
 				);
-		fprintf(*salida, "%s\n", sub->a[i].texto);
+		fprintf(*salida, "%s\n", array->a[i].texto);
 	}
 }
