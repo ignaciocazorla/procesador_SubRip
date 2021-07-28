f32edf3e96f083172f6f5dab2b2c5d2dd49a21c0
Ignacio Cazorla <nacla12@hotmail.com>
2021-07-28 01:02:05 -0300

    Elimina gran parte de los errores. No se arreglo la opcion de insertar (no deberia usarse).

--- args7.c
+++ args7.c
@@ -9,22 +9,26 @@ char *opc[] = {
 	"-o"
 };
 
-static void init_arreglo(struct arreglo_opciones *a, int initial_size) {
-  a->opciones = (struct arr *)malloc(initial_size * sizeof(struct arr));
-  a->tamanio = initial_size;
-  a->ocupado = 0;
+static void init_arreglo(struct arreglo_opciones *arreglo_opciones) {
+	struct arreglo_opciones a = *arreglo_opciones;
+	a.opciones = NULL;
+	a.tamanio = 0;
+  	a.ocupado = 0;
+	*arreglo_opciones = a;
 }
 
-struct arr *insertar_opciones(int opt, int cantidad,...){	
-  struct arr *tmp;
-	va_list ap;
+void insertar_opciones(struct arr * dato, int opt, int cantidad,...){	
+	struct arr s_arr = *dato;
+	
+	va_list ap;	
 	va_start(ap,cantidad);
-	tmp = (struct arr *) malloc(sizeof(struct arr));
-	tmp->opcion = opt;
+
+	s_arr.opcion = opt;
+	
 	if (opt == 1){
-		tmp->args = malloc(sizeof(int ));
-		char *  dato = va_arg(ap,char *);
-		tmp->args = atoi(dato);
+		s_arr.args = malloc(sizeof(int));
+		char * num = va_arg(ap,char *);
+		s_arr.args = atoi(num);
 	}
 	if(opt == 2){ 
 		struct sub * s = malloc (sizeof(struct sub));
@@ -33,39 +37,36 @@ struct arr *insertar_opciones(int opt, int cantidad,...){
 		char * buf = va_arg(ap,char *);
 		s->texto = malloc(strlen(buf));
 		strcpy(s->texto,buf);
-		tmp->args=s;
+		dato->args=s;
 	}
 	if(opt == 3){
-		tmp->args = NULL;
+		s_arr.args = NULL;
 	}
 	if(opt == 4 || opt == 5){
 		char * buf = va_arg(ap,char *);
-		tmp->args = malloc(strlen(buf));
-		strcpy(tmp->args,buf);
+		s_arr.args = malloc(strlen(buf) + 1);
+		strcpy(s_arr.args,buf);
 	}
-	va_end(ap);
-	return tmp;
+	*dato = s_arr;
+
+	va_end(ap);	
+}
+
+void insert_operand(struct arreglo_opciones *a, int opt, int cant_args, char * argv){
+	struct arr dato;
+  	insertar_opciones(&dato, opt, cant_args, argv);
+  	a->tamanio += 1;
+	a->opciones = (struct arr *)realloc(a->opciones, a->tamanio * sizeof(struct arr));
 	
+	(*a).opciones[(*a).ocupado++] = dato;
 }
 
-struct arreglo_opciones *insert_operand(struct arreglo_opciones *a, struct arr * dato){
-  if (a->ocupado == a->tamanio) {
-    a->tamanio *= 2;
-    a->opciones = (struct arr *)realloc(a->opciones, a->tamanio * sizeof(dato));
-  }
-  a->opciones[a->ocupado++] = *dato;
-  return a;
-}
+void recuperar_args(struct arreglo_opciones * arreglo_opciones, int argc, char **argv){	
+	init_arreglo(arreglo_opciones);
 
-
-
-struct arreglo_opciones *recuperar_args(int argc, char **argv){
-	struct arreglo_opciones * arreglo = calloc (1, sizeof(struct arreglo_opciones)); 
-	init_arreglo(arreglo,sizeof(struct arr));
-
-	for(int i = 0;  i< argc ; i++){
-		printf("%s ", argv[i]);
-	}
+	//for(int i = 0;  i< argc ; i++){
+	//	printf("%s ", argv[i]);
+	//}
 
 	int entrada = 0 ,salida = 0;
 	//primer recorrido para recuperar -f y -o (en caso que haya). Voy a guardar estos datos en un arreglo separado para poder trabajar cuando necesite con estos datos.
@@ -74,9 +75,9 @@ struct arreglo_opciones *recuperar_args(int argc, char **argv){
 		//para agregar -f
 		if(strcasecmp (argv[i],"-f") == 0){
 			if(entrada == 0){
-				
-				arreglo = insert_operand(arreglo,insertar_opciones(4,1,argv[i+1]));
-				printf("%s",argv[i+1]);
+
+				insert_operand(arreglo_opciones, 4, 1, argv[i+1]);
+				//printf("%s",argv[i+1]);
 				entrada++;
 			}
 			else{
@@ -88,7 +89,8 @@ struct arreglo_opciones *recuperar_args(int argc, char **argv){
 		//para agregar -o
 		if(strcasecmp (argv[i],"-o") == 0){
 				if(salida == 0){
-					arreglo = insert_operand(arreglo,insertar_opciones(5,1,argv[i+1]));
+					//arreglo = insert_operand(arreglo,insertar_opciones(5,1,argv[i+1]));
+					insert_operand(arreglo_opciones, 5, 1, argv[i+1]);
 					salida++;
 				}
 				else {
@@ -103,10 +105,11 @@ struct arreglo_opciones *recuperar_args(int argc, char **argv){
 		
 	}
 
-	printf("Nombre del archivo de entrada:%s",(char *) arreglo->opciones->args);
-	getchar();
 	
-
+	//printf("Nombre del archivo de entrada:%s",(char *) arreglo->opciones->args);
+	//getchar();
+	
+	
 	//agarrar el resto de los argumentos e imprimir los valores que tiene el supuesto arreglo de las opciones que llegan como argumentos
 	for(int i = 0; i < argc; i++){
 	
@@ -114,32 +117,36 @@ struct arreglo_opciones *recuperar_args(int argc, char **argv){
 		for(optindex = 0; optindex < 3; optindex++){	
 			if(strcasecmp(argv[i],opc[optindex])== 0){
 				if(optindex == 0){		
-					arreglo = insert_operand(arreglo,insertar_opciones(optindex + 1,1,argv[i+1]));
+					//arreglo = insert_operand(arreglo,insertar_opciones(optindex + 1,1,argv[i+1]));
+					insert_operand(arreglo_opciones, optindex + 1, 1, argv[i+1]);
 				}
 				if(optindex == 1){
-					arreglo = insert_operand(arreglo,insertar_opciones(optindex + 1,3,argv[i+1],argv[i+2],argv[i+3]));
+					//arreglo = insert_operand(arreglo,insertar_opciones(optindex + 1,3,argv[i+1],argv[i+2],argv[i+3]));
 				}
 				if(optindex == 2){
-					arreglo = insert_operand(arreglo,insertar_opciones(optindex + 1,1,NULL));
+					//arreglo = insert_operand(arreglo,insertar_opciones(optindex + 1,1,NULL));
+					insert_operand(arreglo_opciones, optindex + 1, 1, argv[i+1]);
 				}
 			}
 		}
 	
 	}
+	/*
 	printf("El arreglo:\n");
 	for(int i = 0; i < arreglo->ocupado;i++){
 		printf("%d ",arreglo->opciones[i].opcion);
 	}
-	printf("\n");
+	printf("\n");*/
 	
-	return arreglo;
+	//return arreglo;
 }
 
 
-void liberar_mem_args(struct arreglo_opciones *a_opciones){
-	for(int i = 0; i <= a_opciones->ocupado ;i++){
-		free(a_opciones->opciones[i].args);
-		free((void *) &(a_opciones->opciones[i]));
+void liberar_mem_args(struct arreglo_opciones * arreglo_opciones){
+	for(int i = 0; i <= arreglo_opciones->ocupado ;i++){
+		if(arreglo_opciones->opciones[i].args != NULL){
+			free(arreglo_opciones->opciones[i].args);
+		}
 	}
-	free(a_opciones);
+	free((*arreglo_opciones).opciones);
 }
